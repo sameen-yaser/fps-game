0360ee50d5f577aceec0b4c1a8ccd3d5fe9e9aae
Saliha Asif <salihasif2002@gmail.com>
2024-04-17 17:23:46 +0500

    sounds and anim

--- FPS_FA20-BCE-030/Assets/Scripts/enemyscript.cs
+++ FPS_FA20-BCE-030/Assets/Scripts/enemyscript.cs
@@ -2,34 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyscript : MonoBehaviour
+public class EnemyScript : MonoBehaviour
 {
     Animator anim;
     public GameObject fps;
-    // Start is called before the first frame update
+    public float detectionRadius = 30f;
+    public float moveSpeed = 5f; // Speed at which the enemy moves
+    public AudioSource dieaudio;
+
+    bool isRunning = true; // Flag to check if enemy is in running state
+
     void Start()
     {
         anim = GetComponent<Animator>();
+        dieaudio = GetComponent<AudioSource>();
         anim.SetBool("isDead", false);
+        anim.SetBool("isTrigger", false);
     }
 
     void Update()
     {
-        transform.LookAt(fps.transform);
+        if (!anim.GetBool("isDead") && !anim.GetBool("isTrigger"))
+        {
+            if (isRunning)
+            {
+                // Move the enemy forward
+                transform.Translate(0, 0, 0.1f);
+            }
+
+            transform.LookAt(fps.transform);
+            if (Vector3.Distance(transform.position, fps.transform.position) <= detectionRadius)
+            {
+                // Switch to shoot animation when player enters the radius 
+                print("triggered");
+                isRunning = false; // Stop running
+                anim.SetBool("isTrigger", true);
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name.StartsWith("bullet"))
+        if (col.gameObject.name.StartsWith("bullet") || col.gameObject.name.StartsWith("grenade"))
         {
             print("hittt");
             anim.SetBool("isDead", true);
+            dieaudio.Play();
         }
-        if (col.gameObject.name.StartsWith("grenade"))
-        {
-            print("hittt");
-            anim.SetBool("isDead", true);
-        }
-
     }
 }
